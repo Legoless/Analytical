7ace32d84ca52d9ec6daa581e24c8eb99bfa4c98
Dal Rupnik <legoless@gmail.com>
2016-11-21 12:28:58 +0100

    Pod update, swift 3 updates

--- Example/Analytical/ViewController.swift
+++ Example/Analytical/ViewController.swift
@@ -3,22 +3,12 @@
 //  Analytical
 //
 //  Created by Dal Rupnik on 07/19/2016.
-//  Copyright (c) 2016 Dal Rupnik. All rights reserved.
+//  Copyright (c) 2016 Unified Sense. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 }
 
