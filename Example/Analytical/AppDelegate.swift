a14d183f606055b63aab237082553f2e4bae35bd
Dal Rupnik <legoless@gmail.com>
2017-05-30 16:55:41 +0200

    Pod update, push notifications working

--- Example/Analytical/AppDelegate.swift
+++ Example/Analytical/AppDelegate.swift
@@ -32,4 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         print("Token: \(deviceToken)")
     }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Error registering: \(error.localizedDescription)")
+    }
 }
