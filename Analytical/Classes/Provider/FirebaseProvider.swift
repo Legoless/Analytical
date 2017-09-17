3e55735133eddbcb1f34923d1e260fb27559eb7e
Dal Rupnik <legoless@gmail.com>
2017-09-17 21:39:47 +0200

    Added base Firebase events

--- Analytical/Classes/Provider/FirebaseProvider.swift
+++ Analytical/Classes/Provider/FirebaseProvider.swift
@@ -103,4 +103,149 @@ public class FirebaseProvider : BaseProvider<Firebase.Analytics>, AnalyticalProv
     public func increment(property: String, by number: NSDecimalNumber) {
         
     }
+    
+    public override func update(event: AnalyticalEvent) -> AnalyticalEvent? {
+        //
+        // Ensure Super gets a chance to update event.
+        //
+        guard var event = super.update(event: event) else {
+            return nil
+        }
+        
+        //
+        // Update event name and properties based on Facebook's values
+        //
+        
+        if let defaultName = DefaultEvent(rawValue: event.name), let updatedName = parse(name: defaultName) {
+            event.name = updatedName
+        }
+        
+        event.properties = prepare(properties: mergeGlobal(properties: event.properties, overwrite: true))
+        
+        return event
+    }
+    
+    //
+    // MARK: Private Methods
+    //
+    
+    private func parse(name: DefaultEvent) -> String? {
+        switch name {
+        case .addedPaymentInfo:
+            return AnalyticsEventAddPaymentInfo
+        case .addedToWishlist:
+            return AnalyticsEventAddToWishlist
+        case .completedTutorial:
+            return AnalyticsEventTutorialComplete
+        case .addedToCart:
+            return AnalyticsEventAddToCart
+        case .viewContent:
+            return AnalyticsEventSelectContent
+        case .initiatedCheckout:
+            return AnalyticsEventBeginCheckout
+        case .campaignEvent:
+            return AnalyticsEventCampaignDetails
+        case .checkoutProgress:
+            return AnalyticsEventCheckoutProgress
+        case .earnCredits:
+            return AnalyticsEventEarnVirtualCurrency
+        case .purchase:
+            return AnalyticsEventEcommercePurchase
+        case .joinGroup:
+            return AnalyticsEventJoinGroup
+        case .generateLead:
+            return AnalyticsEventGenerateLead
+        case .levelUp:
+            return AnalyticsEventLevelUp
+        case .signUp:
+            return AnalyticsEventLogin
+        case .postScore:
+            return AnalyticsEventPostScore
+        case .presentOffer:
+            return AnalyticsEventPresentOffer
+        case .refund:
+            return AnalyticsEventPurchaseRefund
+        case .removeFromCart:
+            return AnalyticsEventRemoveFromCart
+        case .search:
+            return AnalyticsEventSearch
+        case .checkoutOption:
+            return AnalyticsEventSetCheckoutOption
+        case .share:
+            return AnalyticsEventShare
+        case .completedRegistration:
+            return AnalyticsEventSignUp
+        case .spendCredits:
+            return AnalyticsEventSpendVirtualCurrency
+        case .beganTutorial:
+            return AnalyticsEventTutorialBegin
+        case .unlockedAchievement:
+            return AnalyticsEventUnlockAchievement
+        case .viewItem:
+            return AnalyticsEventViewItem
+        case .viewItemList:
+            return AnalyticsParameterItemList
+        case .searchResults:
+            return AnalyticsEventViewSearchResults
+        default:
+            return nil
+        }
+    }
+    
+    private func prepare(properties: Properties?) -> Properties? {
+        guard let properties = properties else {
+            return nil
+        }
+        
+        var finalProperties : Properties = properties
+        
+        for (property, value) in properties {
+            
+            let property = parse(property: property)
+            
+            finalProperties[property] = value
+        }
+        
+        return finalProperties
+    }
+    
+    
+    private func parse(property: String) -> String {
+        switch property {
+        case Property.Purchase.quantity.rawValue:
+            return AnalyticsParameterQuantity
+        case Property.Purchase.item.rawValue:
+            return AnalyticsParameterItemName
+        case Property.Purchase.sku.rawValue:
+            return AnalyticsParameterItemID
+        case Property.Purchase.category.rawValue:
+            return AnalyticsParameterItemCategory
+        case Property.Purchase.source.rawValue:
+            return AnalyticsParameterItemLocationID
+        case Property.Purchase.price.rawValue:
+            return AnalyticsParameterPrice
+        case Property.Purchase.currency.rawValue:
+            return AnalyticsParameterCurrency
+        case Property.Location.origin.rawValue:
+            return AnalyticsParameterOrigin
+        case Property.Location.destination.rawValue:
+            return AnalyticsParameterDestination
+        case Property.startDate.rawValue:
+            return AnalyticsParameterStartDate
+        case Property.endDate.rawValue:
+            return AnalyticsParameterEndDate
+        case Property.Purchase.medium.rawValue:
+            return AnalyticsParameterMedium
+        case Property.Purchase.campaign.rawValue:
+            return AnalyticsParameterCampaign
+        case Property.term.rawValue:
+            return AnalyticsParameterTerm
+        case Property.Content.identifier.rawValue:
+            return AnalyticsParameterContent
+        case Property.User.registrationMethod.rawValue:
+            return AnalyticsParameterSignUpMethod
+        default:
+            return property
+        }
+    }
 }
