33f6b6e17abd6121fb557adc9f3d1be3de488f58
Dal Rupnik <legoless@gmail.com>
2017-12-21 15:33:19 +0100

    Update AnswersProvider.swift

--- Analytical/Classes/Provider/AnswersProvider.swift
+++ Analytical/Classes/Provider/AnswersProvider.swift
@@ -5,14 +5,13 @@
 //  Created by Dal Rupnik on 30/05/17.
 //  Copyright © 2017 Unified Sense. All rights reserved.
 //
-
 import Analytical
-import Answers
+import Crashlytics
 import Fabric
 
 public class AnswersProvider : BaseProvider<Answers>, AnalyticalProvider {
     public func setup(with properties: Properties?) {
-        Fabric.with([ Answers.self ])
+        Fabric.with([ Answers.self, Crashlytics.self ])
     }
     
     public func flush() {
@@ -37,7 +36,29 @@ public class AnswersProvider : BaseProvider<Answers>, AnalyticalProvider {
         case .screen:
             Answers.logContentView(withName: event.name, contentType: "screen", contentId: event.properties?[Property.Content.identifier.rawValue] as? String, customAttributes: properties)
         case .default:
-            Answers.logCustomEvent(withName: event.name, customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            switch event.name {
+            case DefaultEvent.signUp.rawValue:
+                Answers.logSignUp(withMethod: event.properties?[Property.User.registrationMethod.rawValue] as? String, success: NSNumber(value: true), customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            case DefaultEvent.login.rawValue:
+                Answers.logLogin(withMethod: event.properties?[Property.User.registrationMethod.rawValue] as? String, success: NSNumber(value: true), customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            case DefaultEvent.share.rawValue:
+                Answers.logShare(withMethod: nil, contentName: event.properties?[Property.Content.description.rawValue] as? String, contentType: event.properties?[Property.Content.type.rawValue] as? String, contentId: event.properties?[Property.Content.identifier.rawValue] as? String, customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            case DefaultEvent.invite.rawValue:
+                Answers.logInvite(withMethod: nil, customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            case DefaultEvent.achievedLevel.rawValue:
+                Answers.logLevelEnd(event.properties?[Property.Content.identifier.rawValue] as? String, score: NSNumber(value: (event.properties?[Property.Content.identifier.rawValue] as? Double) ?? 0.0), success: NSNumber(value: true), customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            case DefaultEvent.addedToCart.rawValue:
+                Answers.logAddToCart(withPrice: event.properties?[Property.Purchase.price.rawValue] as? NSDecimalNumber, currency: event.properties?[Property.Purchase.currency.rawValue] as? String, itemName: event.properties?[Property.Purchase.item.rawValue] as? String, itemType: event.properties?[Property.Purchase.category.rawValue] as? String, itemId: event.properties?[Property.Purchase.sku.rawValue] as? String, customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            case DefaultEvent.initiatedCheckout.rawValue:
+                Answers.logStartCheckout(withPrice: event.properties?[Property.Purchase.price.rawValue] as? NSDecimalNumber, currency: event.properties?[Property.Purchase.currency.rawValue] as? String, itemCount: NSNumber(value: (event.properties?[Property.Content.identifier.rawValue] as? Int) ?? 0), customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            case DefaultEvent.rating.rawValue:
+                Answers.logRating(nil, contentName: event.properties?[Property.Content.description.rawValue] as? String, contentType: event.properties?[Property.Content.type.rawValue] as? String, contentId: event.properties?[Property.Content.identifier.rawValue] as? String, customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            case DefaultEvent.search.rawValue:
+                Answers.logSearch(withQuery: event.properties?[Property.Content.searchTerm.rawValue] as? String, customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            default:
+                Answers.logCustomEvent(withName: event.name, customAttributes: mergeGlobal(properties: properties, overwrite: true))
+            }
+            
         default:
             super.event(event)
         }
