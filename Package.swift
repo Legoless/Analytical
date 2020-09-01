1e8fda4b58cda3d6404e8427832437a384dbfd4a
Jure Lajlar <jlajlar@gmail.com>
2020-09-01 15:58:31 +0200

    Added swift package file for Analytical/Core.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "Analytical",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_12),
+        .watchOS(.v5)
+    ],
+    products: [
+        .library(
+            name: "Analytical",
+            targets: ["Core"]),
+    ],
+    targets: [
+        .target(
+            name: "Core",
+            path: "Analytical/Classes/Core")
+    ]
+)
